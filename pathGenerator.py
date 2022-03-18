17a44835f55e6dbc8de313dc26d9862123e63c08
Calastrophe <74928681+Calastrophe@users.noreply.github.com>
2022-03-18 12:36:15 -0500

    actually solves all paths

--- pathGenerator.py
+++ pathGenerator.py
@@ -5,7 +5,6 @@ class Node:
         self.nodeNum = node_count
         self.connections = connections
 
-
 def returnConnections(node):
     connections = []
     which_edge = 0
@@ -28,40 +27,28 @@ def returnPath(nodeList, start, end):
     path = [start]
     for node in nodeList:
         if node.nodeNum == start:
-            if end in node.connections:
-                node.connections.remove(end) ## Otherwise you'll just get a singular path...
             scavengeNodes(node, nodeList, end, path, used_nodes)
 
+
+# This will solve all paths that don't make a visit to node twice, larger matrices will take more time.
 def scavengeNodes(node, nodeList, end, cur_path, used_nodes):
-    for search_node in node.connections: # Iterating over our connections to node
-        if search_node not in used_nodes: # If those nodes are not in used_nodes, continue
-            for obj_node in nodeList: # Finding our object
-                if obj_node.nodeNum == search_node: ## Finding our object that matches our search number
-                    curSearch = searchNode(obj_node, end, cur_path) # Search that number for our end node...
-                    if curSearch == None:
-                        used_nodes.append(search_node)
-                        cur_path.append(search_node)
-                        scavengeNodes(obj_node, nodeList, end, cur_path, used_nodes)
-                    else:
-                        used_nodes.append(search_node)
-                        print("New path: ", curSearch)
+    for connection in node.connections: # Iterate over the connections to the node
+        cust_path = cur_path[:]
+        if end == connection:
+            cust_path.append(end)
+            print("New path:", cust_path)
+        for obj_node in nodeList: # Match with the class object that contains its number
+            if obj_node.nodeNum == connection:
+                if obj_node.nodeNum not in cust_path: # Make sure we haven't visited this node yet in our path
+                    cust_path.append(obj_node.nodeNum)
+                    scavengeNodes(obj_node, nodeList, end, cust_path, used_nodes)
 
-def searchNode(node, end, cur_path):
-    cust_path = cur_path[:]
-    if end in node.connections:
-        cust_path.append(node.nodeNum)
-        cust_path.append(end)
-        return cust_path
-    else:
-        return None
-
-
-## No specific algorithim involved, just a linear search of every path possible.
+## No specific algorithim involved. Just handmade.
 ## Unweighted directional graph
 if __name__ == "__main__":
-    edges = np.random.randint(2, size=3600) # Random generation of edges
-    matrix = np.reshape(edges, (60,60)) # Reformation of edges
+    edges = np.random.randint(2, size=81) # Random generation of edges
+    matrix = np.reshape(edges, (9,9)) # Reformation of edges
     nodeList = returnNodeList(matrix)
     for node in nodeList:
         print(node.nodeNum, ": ", node.connections)
-    path = returnPath(nodeList, 1, 40)
+    path = returnPath(nodeList, 1, 3) ## From 1 to 3 paths, make sure your to is included in the size of your matrix if you change this.
